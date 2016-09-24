892be6ba677badc1e4920adf76fd9af14aee2c11
Leipo Yan <leipo.yan@u-blox.com>
2016-09-23 23:55:25 -0700

    Changed input from letters a-g to numbers 1-7

--- composer.sh
+++ composer.sh
@@ -1,43 +1,32 @@
 #!/bin/bash
+TUNEDIR=~/temp/music
+tunes=(
+  "$TUNEDIR/a.wav"
+  "$TUNEDIR/b.wav"
+  "$TUNEDIR/c.wav"
+  "$TUNEDIR/d.wav"
+  "$TUNEDIR/e.wav"
+  "$TUNEDIR/f.wav"
+  "$TUNEDIR/g.wav"
+)
+
+function play_tune
+{
+  echo "num is $1"
+
+  sox ${tunes[$1]} -d &
+}
+
 
 while true ; do
   ## get input and run sanity check
   read digit
-  if [[ "$digit" > "g" ]] || [[ "$digit" < "a" ]] ; then
+  if [[ "$digit" > "7" ]] || [[ "$digit" < "1" ]] ; then
     echo " Invalid input"
     continue;
   fi
-  echo "You selected $digit ($idx)" 
+  echo "You selected $digit $(($digit-1))" 
   
-  case $digit in
-  'a')
-    sox a.wav -d &
-    ;;
-  'b')
-    sox b.wav -d &
-    ;;
-  'c')
-    echo "this is c"
-    sox c.wav -d &
-    ;;
-  'd')
-    echo "this is d"
-    sox d.wav -d &
-    ;;
-  'e')
-    echo "this is e"
-    sox e.wav -d &
-    ;;
-  'f')
-    echo "this is f"
-    sox f.wav -d &
-    ;;
-  'g')
-    echo "this is g"
-    sox g.wav -d &
-    ;;
-  *)
-    echo "this is others"
-    ;;
-  esac
+  play_tune $(($digit-1))
+
 done
