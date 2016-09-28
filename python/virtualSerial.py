4d5cc9362629a3d030158affa84a9ce0f334d252
Leipo Yan <leipo.yan@u-blox.com>
2016-09-28 06:05:38 -0700

    Python script for creating virtual serial port

---
+++ python/virtualSerial.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import os, pty, serial
+
+master, slave = pty.openpty()
+s_name = os.ttyname(slave)
+
+ser = serial.Serial(s_name)
+
+# To Write to the device
+ser.write('Your text')
+
+# To read from the device
+buf = os.read(master,1000)
+print ("data read is: " + buf)
