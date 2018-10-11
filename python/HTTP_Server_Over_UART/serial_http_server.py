afc7fa42a52046fad0aa4203f4de6501a12bd796
Leipo Yan <leipo.yan@u-blox.com>
2018-10-11 14:59:40 +0800

    Complete HTTP Server Over UART

--- python/HTTP_Server_Over_UART/serial_http_server.py
+++ python/HTTP_Server_Over_UART/serial_http_server.py
@@ -1,37 +1,14 @@
-# <html><body>
-# <h1>You need to update your python script settings!</h1>        <br />
-# <h4>If you're seeing this page in the web browser then you      <br />
-# need to configure the following variables in the python script: <br /><br />
-
-# Change this variable to point to your webpage files directory   <br />
-ROOT_SERVER_PATH = '.'                                           #<br /><br />
-
-# This should be the default webpage (i.e. index.html)           <br />
-#ROOT_INDEX = 'serial_http_server.py'
-ROOT_INDEX = 'index.html'
-
-# <br /><br /><br /><br /></h4></body></html>
-
-SOFTAP_SSID = "HTTP Server Demo"
-SOFTAP_PASSWORD = "password"
-HTTP_URL = "demo.com"
-
-
-
 import sys
-try:
-    import serial
-except Exception as e:
-    print "Failed to import 'serial' python module"
-    print "This may be installed with the command: 'pip install serial'"
-    print "It may also be found here: https://pypi.python.org/pypi/pyserial"
-    sys.exit(1)
-    
+import serial
 import time
 import os
 import random, sys
 
-
+ROOT_SERVER_PATH = r'.'
+ROOT_INDEX = 'index.html'
+SOFTAP_SSID = "UBXWifi"
+SOFTAP_PASSWORD = "topsecret"
+HTTP_URL = "http://192.168.2.1:8080"
 
 MAX_READ_LENGTH  = 65000
 
@@ -52,10 +29,18 @@ HTTP_NOT_FOUND  = "<html>\r\n"+\
                   "</body>\r\n"+\
                   "</html>"
 
+HTTP_CREDENTIAL = "<html>\r\n"+\
+                  "<body>\r\n"+\
+                  "Credential recorded for <h1>SSID: %s</h1>\r\n"+\
+                  "</body>\r\n"+\
+                  "</html>"
+
+SSID_QUERY_KEY = "SSID"
+PASSWORD_QUERY_KEY = "psw"
                   
 if len(sys.argv) != 2:
     print "Must specify the serial COM port"
-    print "ex: python serial_http_server.py COM35"
+    print "ex: python serial_http_server.py COM30"
     sys.exit(1)
 
 print "Opening serial port: %s" % sys.argv[1]
@@ -81,57 +66,129 @@ class HTTPRequest(BaseHTTPRequestHandler):
         self.error_code = code
         self.error_message = message
 
-
 ''' ******************************************************************************************* '''
-def getCmdResponse(printRes=True):
-    global s
-    header = ""
-    while True:
-        while len(header) == 0 or (header[0] != 'R' and header[0] != 'L'):
-            header = s.readline().strip()
-            if len(header) > 0 and (ord(header[0]) > 126 or ord(header[0]) < 32):
-                header = header[1:]
-            if '[Ready]' in header:
-                return None
-        if printRes:
-            print "<- %s" % header
-        statusCode = header[1]
-        l = int(header[2:])
-        if l > 0:
-            data = s.read(l)
-            if printRes or statusCode != '0':
-                print "<- %s" % data
-            if header[0] == 'R' :
-                if statusCode == '0':
-                    return data[:-2]
-                else:
-                    #raise Exception("Command failed %s" % statusCode)
-                    print "Command failed"
-                    return ""
-            else:
-                header = ""
-        else:
-            return None
+def atCommand(command):
+    try:
+        s.write(command)
+        time.sleep(.050)
+        cmdComplete=False
+        if(command == "+++"):
+            return
             
+        while(cmdComplete==False):
+            while(s.inWaiting()):
+                result = s.read(s.inWaiting())
+                print result
+                if( (result.find('OK')!=-1) or (result.find('ERROR')!=-1)):
+                    cmdComplete=True
+                else:
+                    sleep(0.05)
+        #while(s.inWaiting()):
+        #    result = s.read(s.inWaiting())
+        #    print result
+            
+    except Exception, e:
+        print "Exception for AT comand. Type is:",e.__class__.__name__
+
+def commandMode():
+    time.sleep(1)
+    atCommand("+++")
+    time.sleep(1)
+
+def dataMode():
+    atCommand("ATO1\r\n")
+
+def factoryReset():
+    atCommand("AT+UFACTORY\r\n")
+    atCommand("AT+CPWROFF\r\n")
+    time.sleep(3)
+
+def setupAP():
+    commandMode()
+    factoryReset()
+    atCommand("AT+UDSC=1,1,8080,0\r\n")
+    atCommand("AT+UMSM=1\r\n")
+    atCommand("AT&D0\r\n")
+    atCommand("AT+UWAPC=0,16,0\r\n")  #disable hidden SSID
+    atCommand("AT+UWAPC=0,0,1\r\n")
+    atCommand("AT+UWAPC=0,2," + SOFTAP_SSID + "\r\n")
+    atCommand("AT+UWAPC=0,4,6\r\n")
+    atCommand("AT+UWAPC=0,5,3,2\r\n")
+    atCommand("AT+UWAPC=0,8," + SOFTAP_PASSWORD + "\r\n")
+    atCommand("AT+UWAPC=0,14,0\r\n")
+    atCommand("AT+UWAPC=0,100,1\r\n")
+    atCommand("AT+UWAPC=0,101,192.168.2.1\r\n")
+    atCommand("AT+UWAPC=0,102,255.255.255.0\r\n")
+    atCommand("AT+UWAPC=0,103,192.168.2.1\r\n")
+    atCommand("AT+UWAPC=0,104,8.8.8.8\r\n")
+    atCommand("AT+UWAPC=0,107,0\r\n")
+    atCommand("AT+UWAPC=0,106,1\r\n")
+    atCommand("AT+UWAPCA=0,3\r\n")
+    dataMode()
+
+def connectToAP(SSID,pwd):
+    commandMode()
+    atCommand("AT+UWAPCA=0,4\r\n")
+    # Deactiving AP while there are active STAs will cause the module unable to function as STA.
+    # WA: Deactive AP, Active it and deactive it again.
+    time.sleep(1)
+    atCommand("AT+UWAPCA=0,3\r\n")
+    atCommand("AT+UWAPCA=0,4\r\n")
+    time.sleep(1)
+    atCommand("AT+UWSC=0,0,0\r\n")
+    atCommand("AT+UWSC=0,2," + SSID + "\r\n");
+    atCommand("AT+UWSC=0,5,2\r\n")
+    atCommand("AT+UWSC=0,8," + pwd + "\r\n")
+    atCommand("AT+UWSC=0,100,2\r\n")
+    atCommand("AT+UWSC=0,107,0\r\n")
+    atCommand("AT+UWSC=0,300,0\r\n")
+    atCommand("AT+UWSC=0,301,1\r\n")
+    atCommand("AT+UWSCA=0,3\r\n")
+    time.sleep(20)
+    atCommand("AT+UNSTAT\r\n")
+    atCommand("AT+UWSSTAT\r\n")
+    dataMode()
+
+def checkNetworkStatus():
+    commandMode()
+    time.sleep(5)
+    atCommand("AT+UNSTAT\r\n")
+    dataMode()
+    
+''' ******************************************************************************************* '''
+def extractPostContent(message):
+    try:
+        #request = HTTPRequest(message)
+        startPos = message.find('\r\n\r\n')+4
+        endPos = len(message)
+        content = message[startPos:endPos]
+        return content
+    except Exception:
+        print "Unable to extract content from post message"
+
 
 ''' ******************************************************************************************* '''
-def sendCommand(cmd, printRes=True, printCmd=True):
-    global s
-    if printCmd:
-        print "-> %s" % cmd
-    s.write(cmd + "\r\n")
+def queryExtract(query, key):
+    try:
+        tokens = query.split('&')
+        for i in range(len(tokens)):
+            if tokens[i].find(key)!= -1:
+                keyVal = tokens[i][len(key)+1:len(tokens[i])]
+                return keyVal
+    except Exception:
+        print "Unable to extract key " + key
 
-    return getCmdResponse(printRes)
 
 
 ''' ******************************************************************************************* '''
 def sendHttpResponse(clientId, fname):
     global s
     
-    if fname == '/':
+    if (fname == '/') or (fname == '') :
         fname = ROOT_INDEX
 
     fname = os.path.join(ROOT_SERVER_PATH, fname)
+    print ">>> Leipo: fname: " + fname + "..."
     
     if(os.path.isfile(fname)):
         file = open(fname, 'r+b')
@@ -148,10 +205,6 @@ def sendHttpResponse(clientId, fname):
             
         header = HTTP_HEADER % (contentType, size)
 
-        # prepare http response and send to client
-        cmd = "stream_write %d %d" % (clientId, len(header) + size)
-        print "-> %s" % cmd
-        s.write(cmd + "\r\n")
         
         # write http response data to client 
         s.write(header)
@@ -159,15 +212,12 @@ def sendHttpResponse(clientId, fname):
         while readSize < size:
             chunk = file.read(min(128, size- readSize))
             readSize = readSize + len(chunk)
-            #print "Total read size = %d " % readSize
             if chunk:
-               #print "Writing chunk of %d bytes" % len(chunk)
                s.write(chunk)
                time.sleep(0.05);
             else:
                 break
 
-        getCmdResponse(False)
         print  "Response sent to client %d" %clientId
 
     else:
@@ -176,50 +226,40 @@ def sendHttpResponse(clientId, fname):
         print "*****************************"
         # prepare http response (404) and send to client
         header = HTTP_HEADER % ('text/html', len(HTTP_NOT_FOUND))
-        cmd = "stream_write %d %d" % (clientId, len(header) + len(HTTP_NOT_FOUND))
-        print "-> %s" % cmd
-        s.write(cmd + "\r\n")
         
         s.write(header)
         # write http response (404) data to client 
         s.write(HTTP_NOT_FOUND)
-        getCmdResponse(False)
         print  "Response (404) sent to client %d" %clientId
-        
-        
+
+
+''' ******************************************************************************************* '''
+def processHttpPost(clientId, message):
+    global s
+    content = extractPostContent(message)
+    print ("Leipo: content ", content)
+    SSID=str(queryExtract(content, SSID_QUERY_KEY))
+    password=str(queryExtract(content, PASSWORD_QUERY_KEY))
+    
+    responseMsg = HTTP_CREDENTIAL % SSID
+    
+    # prepare http response and send to client
+    header = HTTP_HEADER % ('text/html', len(responseMsg))
+    
+    s.write(header)
+    # write http response data to client 
+    s.write(responseMsg)
+    print  "Post Response sent to client %d" %clientId
+    time.sleep(1)
+    connectToAP(SSID,password)
+    
 
 ''' ******************************************************************************************* '''
 # --------------------- MAIN -------------------------
 try:
-    clientList = {}
     
     # set machine mode of the module
-    sendCommand("set system.cmd.mode machine")
-    sendCommand("set system.print_level all")
-    sendCommand("set bus.log_bus uart0")
-    sendCommand("set network.buffer.size 25000")
-    sendCommand("set network.buffer.rxtx_ratio 50")
-    sendCommand("set bus.command.rx_bufsize 2048")
-    sendCommand("set tcp.keepalive.enabled 0")
-    sendCommand("set tcp.keepalive.initial_timeout 7")
-    sendCommand("set tcp.keepalive.retry_count 3")
-    sendCommand("set tcp.keepalive.retry_timeout 3")
-    sendCommand("set tcp.server.auto_start 1")
-    sendCommand("set tcp.server.auto_interface softap")
-    sendCommand("set tcp.server.port 80")
-    sendCommand("set tcp.server.idle_timeout 30")
-    sendCommand("set tcp.server.max_clients 4")
-    sendCommand("set softap.auto_start 1")
-    sendCommand("set softap.ssid \"%s\"" % SOFTAP_SSID)
-    sendCommand("set softap.passkey \"%s\"" % SOFTAP_PASSWORD)
-    #sendCommand("set softap.url %s,www.%s" % (HTTP_URL, HTTP_URL)) # renamed in ZentriOS 3.0
-    sendCommand("set softap.dns_server.url %s,www.%s" % (HTTP_URL, HTTP_URL))    
-    sendCommand("set stream.auto_close 0")
-    sendCommand("save")
-    sendCommand("get all")    
-    sendCommand("reboot")
-    time.sleep(3)
-    
+    setupAP()
 
     print "Serial HTTP Server Ready ..."
     print "Connect your computer to the Wi-Fi network: %s" % SOFTAP_SSID
@@ -228,74 +268,14 @@ try:
     print "This will display your webpages ..."
     
     while(True):
-        # poll all the connections for their current status
-        # NOTE: this makes the assumption that all connections
-        #       are TCP server connections
-        poll = sendCommand('poll all', False, False)
-        
-        # if there are no open connections then just sleep for a bit
-        # and try again later
-        if poll == 'None' or poll.find(',') == -1:
-            time.sleep(1)
-            continue
-            
-        # split the poll into entries
-        connList = []
-        for x in poll.split('|'):
-            x = x.split(',')
-            connList.append({'handle' : int(x[0]), 'status' : int(x[1])})
-            
-        # for each connection in the polled list
-        for conn in connList:
-            # has the client closed the connection?
-            if conn['status'] == STATUS_CLOSED:
-                # yes, so close the connection
-                print "Client status closed, closing: %d" % conn['handle']
-                sendCommand('close %d' % conn['handle'])
-                # remove from list (if necessary)
-                if conn['handle'] in clientList:
-                    del clientList[conn['handle']]
-                # continue to next connection
-                continue
-            
-            # is this client in our list?
-            if not conn['handle'] in clientList:
-                # no, add it now
-                clientList[conn['handle']] = ''
-                
-            # has the client's response already been sent?
-            # if so have we wait long enough for it to recieve the response?
-            if isinstance(clientList[conn['handle']], float):
-                if time.time() - clientList[conn['handle']] > 20.0:
-                    # yes, so force the connection closed
-                    print "Forcing client connection closed: %d" % conn['handle']
-                    del clientList[conn['handle']]
-                    sendCommand('close %d' % conn['handle'])
-                    continue
-    
-            # does the client have data?
-            if conn['status'] != STATUS_DATA:
-                # no, continue to next connection
-                continue 
-            
-            # yes, so read it now
-            data = sendCommand('read %d %d' % (conn['handle'], MAX_READ_LENGTH), False)
-            if data != None:
-                # buffer the HTTP request data
-                clientList[conn['handle']] += data
-                
-            # Have we received \r\n\r\n ? 
-            # If so then the client sent a full HTTP request
-            if clientList[conn['handle']].find('\r\n\r\n') == -1:
-                # no, haven't received full request yet, continue to next client
-                continue
-
             # parse the client request
-            requestData = clientList[conn['handle']]
+            requestData = ""
+            buffer = ""
+            while(s.inWaiting()):
+                buffer = s.read(s.inWaiting())
+                requestData += buffer
             request = HTTPRequest(requestData)
-            
-            # check if this is a GET request to send response
-            if(request.command.lower() == "get"):
+            if(str(request.command).lower() == "get"):
                 print "----------------------------"
                 print "Request Cmd: %s" % request.command
                 print "----------------------------"
@@ -303,12 +283,20 @@ try:
                 print "----------------------------"
                 print "Request Str: %s" % requestData.strip()
                 print "----------------------------"
+                sendHttpResponse(0, request.path.lstrip('/'))
                 
-                sendHttpResponse(conn['handle'], request.path)
-                
-                # wait a bit and close the connection if the client doesn't
-                clientList[conn['handle']] = time.time()
-                
+            if(str(request.command).lower() == "post"):
+                print "----------------------------"
+                print "Request Cmd: %s" % request.command
+                print "----------------------------"
+                print "Request Pth: %s" % request.path
+                print "----------------------------"
+                print "Request Str: %s" % requestData
+                print "----------------------------"
+                processHttpPost(0,requestData)
+            
+            time.sleep(0.2)
+            
 except Exception as e:
     print "Exception: %s" % e
     raise
