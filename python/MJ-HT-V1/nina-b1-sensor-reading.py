a382185271838e38120bc4df22cc15504348f7c5
Leipo Yan <leipo.yan@u-blox.com>
2019-03-28 14:18:44 +0800

    Update nina-b1-sensor-reading.py script

--- python/MJ-HT-V1/nina-b1-sensor-reading.py
+++ python/MJ-HT-V1/nina-b1-sensor-reading.py
@@ -6,31 +6,7 @@ import threading
 import sys
 import argparse
 import signal
-
-usage = 'Read AT command from config file and process it. Use - for command from stdin (CTRL+D to exit).'
-parser = argparse.ArgumentParser(description=usage)
-parser.add_argument('com_port')
-parser.add_argument('cmdfile', nargs='?', type=argparse.FileType('r'), default=sys.stdin,
-                    help='config file with AT command')
-parser.add_argument('logfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout,
-                    help='log file')
-
-args = parser.parse_args()
-com_port = args.com_port
-
-r_ser = serial.Serial(
-  port = com_port,
-  baudrate = 115200,
-  bytesize = serial.EIGHTBITS,
-  parity = serial.PARITY_NONE,
-  stopbits = serial.STOPBITS_ONE,
-  timeout = 0,
-  xonxoff = False,
-  rtscts = True,
-  dsrdtr = False,
-  write_timeout = None,
-  inter_byte_timeout = None
-)
+import queue
 
 #--------------------------------------------------------
 # ThreadMonitor class
@@ -63,72 +39,133 @@ class Logger(object):
        self.logfile.write('[' + time_text + '] ' + string)
     else:
        self.logfile.write('[' + time_text + '] ' + string.decode('utf-8'))
-    #if(self.logfile != sys.stdout) : print (string)
-    
+
     return
 
+        
+#--------------------------------------------------------
+# Commander class
+#--------------------------------------------------------
+class Commander(object):
+  def __init__(self, cmdfile):
+    self.cmdfile = cmdfile
+    return
+
+  def cmd(self, cmdString):
+    self.cmdString = cmdString.rstrip()+'\r\n'
+    return(self.cmdString)
+
+  def get_command(self):
+    while 1:
+        #try:
+        cmdString = self.cmdfile.readline()
+        if(cmdString != ''):
+          self.cmd(cmdString)
+          return (self.cmdString)
+        else:
+          self.cmdfile.seek(0)
+          continue
+      
+
+#--------------------------------------------------
+# event class
+#--------------------------------------------------
+class Event():
+  class EVENT_TYPE:
+    UNKNOWN = 0
+    STARTUP = 1
+    CMD_OK = 2
+    CMD_ERROR = 3
+    GATT_ACL_CONNECTED = 4
+    GATT_ACL_DISCONNECTED = 5
+    GATT_NOTIFICATION = 6
+    
+  def __init__( self ):
+    return
+
+  def get_event_type( self ):
+    raise NotImplementedError
+
+class StartupEvent( Event ):
+  def get_event_type( self ):
+    return Event.EVENT_TYPE.STARTUP
+
+class CMDOkEvent( Event ):
+  def get_event_type( self ):
+    return Event.EVENT_TYPE.CMD_OK
+
+class CMDErrorEvent( Event ):
+  def get_event_type( self ):
+    return Event.EVENT_TYPE.CMD_ERROR
+    
+class GATTACLConnectedEvent( Event ):	
+  def get_event_type( self ):
+    return Event.EVENT_TYPE.GATT_ACL_CONNECTED
+
+class GATTACLDisconnectedEvent( Event ):
+  def get_event_type( self ):
+    return Event.EVENT_TYPE.GATT_ACL_DISCONNECTED
+
+class GATTNotificationEvent( Event ):
+  def __init__( self, payload ):
+    pos = payload.rfind(',')
+    self.payload = payload[pos+1:]
+    try:
+      print(bytes.fromhex(self.payload).decode('utf-8'))
+    except:
+      pass
+
+  def get_event_type( self ):
+    return Event.EVENT_TYPE.GATT_NOTIFICATION
+    
 #--------------------------------------------------------
 # Parser class
 #--------------------------------------------------------
 class Parser(object):
   def __init__(self):
-    print ("Parser init")
-
-  def parse(self, buf):
-    posStart = buf.find('+UUBTGN:')
-    if(posStart != -1) :
-        remain = buf[posStart:]
-        posEnd = remain.find('\r\n')
-        payload = remain[13:posEnd]
-        try:
-          print(bytes.fromhex(payload).decode('ascii'))
-        except:
-          pass
-    
-#--------------------------------------------------------
-# Command class
-#--------------------------------------------------------
-class Command(object):
-  def __init__(self, serial_port):
-    self.serial_port = serial_port
-    return
-
-  def execute(self):
-    raise NotImplementedError
-
-  def at_mode(self):
-    time.sleep(1)
-    self.serial_port.write( b'+++' )
-    time.sleep(1)
-    print ("AT command mode")
-    return
-
-  def data_mode(self):
-    self.serial_port.write( b'ATO1\r\n' )
-    print ("Data mode")
-    return
-
-  def cmd(self, cmdString):
-    if(cmdString.find("at_mode") != -1): 
-       self.at_mode()
-    elif(cmdString.find("wait") != -1): 
-       time.sleep(0.5)
+    self.cmd_event_queue = queue.Queue(maxsize=50)
+    self.status_event_queue = queue.Queue(50)
+  
+  def process_serial_data(self, buf):
+    logger.log(buf)
+    buf = buf.strip();
+    if(buf.startswith('OK')):
+      try:
+        self.cmd_event_queue.put(CMDOkEvent())
+      except queue.Full:
+        pass
+    elif(buf.startswith('ERROR')):
+      try:
+        self.cmd_event_queue.put(CMDErrorEvent())
+      except queue.Full:
+        pass
+    elif(buf.startswith('+UUBTACLC:')):
+      try:
+        self.status_event_queue.put(GATTACLConnectedEvent())
+      except queue.Full:
+        pass
+    elif(buf.startswith('+UUBTACLD:')):
+      try:
+        self.status_event_queue.put(GATTACLDisconnectedEvent())
+      except queue.Full:
+        pass
+    elif(buf.startswith('+UUBTGN:')):
+      try:
+        self.status_event_queue.put(GATTNotificationEvent(buf))
+      except queue.Full:
+        pass
+    elif(buf.startswith('AT') or buf.startswith('at') or buf == ''):
+      pass
     else:
-       self.cmdString = cmdString[:len(cmdString)-1]+'\r\n'
-       #print ("Send cmd ", self.cmdString)
-       #print ("Send encoded cmd ", self.cmdString.encode('utf-8'))
-       self.serial_port.write( self.cmdString.encode('utf-8') )
-
+      pass
+      
 #--------------------------------------------------------
 # Sender class
 #--------------------------------------------------------
 class Sender(object):
-  def __init__(self, serial_port, cmdfile, logger):
-    self.serial_port = serial_port
-    self.cmdfile = cmdfile 
-    self.logger = logger
-    self.start_time = time.time()
-    self.command = Command(self.serial_port)
+  def __init__(self, controller):
+    self.serial_port = controller.serial_port
+    self.parser = controller.parser
     self.stop_flag = False
     self.thread = threading.Thread(target = self.run)
     self.thread.start()
@@ -139,25 +176,28 @@ class Sender(object):
     return
   
   def run(self):
-    print("If AT command from stdin, use CTRL+D to exit")
-    ##self.command.at_mode()
-
     while not self.stop_flag:
-      cmdString = self.cmdfile.readline()
-      if (len(cmdString) > 1) : self.command.cmd(cmdString)
-      if (len(cmdString) == 0) : 
-        self.cmdfile.seek(0)
-      time.sleep(0.05)
+      cmdString = commander.get_command()
+      if(cmdString!=''):
+        if(cmdString.find('wait') != -1):
+          time.sleep(1)
+          continue
+          
+        self.serial_port.write(cmdString.encode('utf-8'))
+        try:
+          event = self.parser.cmd_event_queue.get(block=True, timeout=2)
+        except queue.Empty:
+          pass
     return
+
       
 #--------------------------------------------------------
 # Receiver class
 #--------------------------------------------------------
 class Receiver(object):
-  def __init__(self, serial_port, logger):
-    self.serial_port = serial_port
-    self.logger = logger
-    self.parser = Parser()
+  def __init__(self, controller):
+    self.serial_port = controller.serial_port
+    self.parser = controller.parser
     self.buf = ''
     self.stop_flag = False
     self.thread = threading.Thread(target = self.run)
@@ -170,33 +210,98 @@ class Receiver(object):
 
   def run(self):
     while ((not self.stop_flag)) :
-      buf = ''
-      if( self.serial_port.in_waiting > 0) :
-        buf = self.serial_port.read(self.serial_port.in_waiting).decode('ascii')
-        self.logger.log(buf)
-        self.buf += buf
-      
-      if(buf.find('\r\n')!=-1):
-        self.parser.parse(self.buf)
+      '''
+      buf = self.serial_port.readline().decode('utf-8')
+      if (buf != ''):
+        self.parser.process_serial_data(buf)
+      '''
+      c = self.serial_port.read(1)
+      if(c == b''):
+        continue
+      if(c != b'\n'):
+        self.buf += c.decode('utf-8')
+      else:
+        self.parser.process_serial_data(self.buf)
         self.buf = ''
-        
-      time.sleep(0.05)
+
     return
+
+#--------------------------------------------------
+# controller class
+#--------------------------------------------------
+class Controller():
+  def __init__( self, serial_port):
+    self.serial_port = serial_port
+    self.parser = Parser();
+    self.thread = threading.Thread( target = self.run )
+    self.thread.setDaemon( True )
+    self.thread.start()
+    return
+    
+  def run( self ):
+    self.receiver = Receiver( self )
+    self.sender = Sender( self )
+    while 1:
+      try:
+        event = self.parser.status_event_queue.get(block=True, timeout=None)
+      except queue.Empty:
+          pass
+    return
+
+#--------------------------------------------------
+# main function
+#--------------------------------------------------
+usage = 'Read AT command from config file and process it. Use - for command from stdin (CTRL+D to exit).'
+parser = argparse.ArgumentParser(description=usage)
+
+parser.add_argument('com_port')
+
+parser.add_argument('cmdfile', 
+                    nargs='?', 
+                    type=argparse.FileType('r'), 
+                    default=sys.stdin,
+                    help='config file with AT command')
+                    
+parser.add_argument('logfile', 
+                    nargs='?', 
+                    type=argparse.FileType('w'), 
+                    default=sys.stdout,
+                    help='log file')
+
+args = parser.parse_args()
+
+com_port = args.com_port
+
+r_ser = serial.Serial(
+  port = com_port,
+  baudrate = 115200,
+  bytesize = serial.EIGHTBITS,
+  parity = serial.PARITY_NONE,
+  stopbits = serial.STOPBITS_ONE,
+  timeout = 0,
+  xonxoff = False,
+  rtscts = True,
+  dsrdtr = False,
+  write_timeout = None,
+  inter_byte_timeout = None
+)
 
 # Create threadmonitor instance
 threadMonitor = ThreadMonitor()
 
-# Create receiver instance
-receiver = Receiver(r_ser, Logger(args.logfile))
+# Create Logger
+logger = Logger(args.logfile)
 
-# Create sender instance
-sender = Sender(r_ser, args.cmdfile, Logger(args.logfile))
+# Create Commander
+commander = Commander(args.cmdfile)
+
+# Create Controller
+controller = Controller(r_ser)
 
 try:
   while 1:
     time.sleep(1)
 except KeyboardInterrupt:
   print("Exiting program")
-  sender.stop()
-  receiver.stop()
+
 
