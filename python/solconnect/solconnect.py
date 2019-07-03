6ed9bde5a0659f3934c0454b734eb1117460aef1
Leipo Yan <leipo.yan@u-blox.com>
2019-07-03 13:35:50 +0800

    Add time to message content

--- python/solconnect/solconnect.py
+++ python/solconnect/solconnect.py
@@ -1,5 +1,6 @@
 import paho.mqtt.client as mqtt
 import time
+from datetime import datetime
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, rc):
@@ -16,16 +17,16 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.username_pw_set(username="solace-cloud-client",password="")
-client.connect("mrjfgwkeg17pb.messaging.solace.cloud", 20806, 60)
+#client.username_pw_set(username="solace-cloud-client",password="")
+client.connect("192.168.1.36", 1883, 60)
 
 
 # Avoiding block to custmize publishing
 client.loop_start()    
 time.sleep(1)
 count=1
-while count < 10:
-    client.publish("try-me", "message {}".format(count))
+while count < 100:
+    client.publish("try-me", "message {} at {}".format(count,datetime.now()))
     count = count+1
-    time.sleep(1)
+    time.sleep(0.5)
 client.loop_stop()
