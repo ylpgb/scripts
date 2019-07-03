e90fba26549508557239b289bc56b94452ebe523
Leipo Yan <leipo.yan@u-blox.com>
2019-07-03 10:12:01 +0800

    Add Solace connect client for python

---
+++ python/solconnect/solconnect.py
@@ -0,0 +1,31 @@
+import paho.mqtt.client as mqtt
+import time
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, rc):
+    print("Connected with result code "+str(rc))
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("try-me")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print("Message received. Topic: " + msg.topic+" "+str(msg.payload))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.username_pw_set(username="solace-cloud-client",password="")
+client.connect("mrjfgwkeg17pb.messaging.solace.cloud", 20806, 60)
+
+
+# Avoiding block to custmize publishing
+client.loop_start()    
+time.sleep(1)
+count=1
+while count < 10:
+    client.publish("try-me", "message {}".format(count))
+    count = count+1
+    time.sleep(1)
+client.loop_stop()
