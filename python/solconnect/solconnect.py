1d1ecb63c01ef1b79779a24f87d48888ef0620ef
Leipo Yan <leipo.yan@solace.com>
2019-10-04 13:52:27 +0800

    Updated test script for paho-mqtt version 1.4.0. The on_connect function on previous script is not called with new version as there is a missing parameter flags.

--- python/solconnect/solconnect.py
+++ python/solconnect/solconnect.py
@@ -3,7 +3,7 @@ import time
 from datetime import datetime
 
 # The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, rc):
+def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
@@ -18,14 +18,16 @@ client.on_connect = on_connect
 client.on_message = on_message
 
 #client.username_pw_set(username="solace-cloud-client",password="")
-client.connect("192.168.1.36", 1883, 60)
+#client.connect("lpy.ddns.net", 1883, 60)
+client.connect("192.168.40.130", 1883, 60)
 
-
+print("Starting loop...")
 # Avoiding block to custmize publishing
 client.loop_start()    
 time.sleep(1)
 count=1
 while count < 100:
+    print("Send message ", count)
     client.publish("try-me", "message {} at {}".format(count,datetime.now()))
     count = count+1
     time.sleep(0.5)
