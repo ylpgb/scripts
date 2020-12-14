143a234e1cb48eaa0bfb60e01d5109d762fbc913
Admin <admin@Admins-MacBook-Pro.local>
2020-12-14 17:57:09 +0800

    Add convenience semp script

---
+++ bash/solace/semp/delete_queue.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+MANAGEMENT_USER=admin
+MANAGEMENT_PASSWORD=admin
+BROKER_SEMP_URL=192.168.40.11:80
+#BROKER_SEMP_URL=cloud.ylpsingapore.com:8080
+MESSAGE_VPN=default
+
+for i in {1..3000}; do
+   curl -X DELETE -u $MANAGEMENT_USER:$MANAGEMENT_PASSWORD http://$BROKER_SEMP_URL/SEMP/v2/config/msgVpns/$MESSAGE_VPN/queues/test_q$i
+done
+
