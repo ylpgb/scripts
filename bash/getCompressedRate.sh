33aca8f2f2db42ee666baafe3c415fc447e5a721
Admin <admin@Admins-MacBook-Pro.local>
2021-02-13 20:39:47 +0800

    Update

--- bash/getCompressedRate.sh
+++ bash/getCompressedRate.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
-clientList="
-GBRPSM020005372/1304/#00000001
-GBRPSM020005372/1304/#00000001
-GBRPSM020005372/2360/#00000001
-GBRPSM020005372/3332/#00000001
-GBRPSM020005372/5388/#00000001
-GBRPSM020005372/2668/#00000001
-GBRPSM020005372/6632/#00000001
-GBRPSM020005372/7580/#00000001
-GBRPSM020005372/7836/#00000001
-"
 
+logfile=$1
 
+[[ $# < 1 ]] && { echo "Specify stat file"; exit 1; }
+
+clientList=`grep '^GBRPSM02000' $logfile | cut -d' ' -f1 | sort | uniq`
+
+sum=0
 for client in $clientList; do 
   awkRegExp=$(echo $client | sed 's|/|\\/|g')
   #echo "$awkRegExp"
 
-  cmd="awk '/^$awkRegExp/ && \$25>=60 {p=1}; p; /^show client GBRPSM020005372.*connections wide/ {p=0}' rt47936-stats.txt | grep 'Client Name.*$client' -A100 | grep 'Compressed Rate (60 sec)' | awk 'BEGIN{max=0} {if (\$5>max) max=\$5 } END{print max*8}'"
+  cmd="awk '/^$awkRegExp/ && \$25>=60 {p=1}; p; /^show client GBRPSM020005372.*connections wide/ {p=0}' $logfile | grep 'Client Name.*$client' -A100 | grep 'Compressed Rate (60 sec)' | awk 'BEGIN{max=0} {if (\$5>max) max=\$5 } END{print max*8}'"
   
   ret=`eval $cmd`
+  sum=$(($sum + $ret))
+
   echo "Client: $client: $ret bps"
 done
-
+echo "Total: $sum bps"
 
