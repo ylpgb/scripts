beab0c6e826132954e14731d13f30a75a1b257e5
Leipo Yan <lyan@maxlinear.com>
2023-10-16 16:51:02 +0800

    Allocate a chunk of memory and get its physical address

--- kernel_module/lkm_example.c
+++ kernel_module/lkm_example.c
@@ -1,41 +1,39 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/slab.h>
+#include <asm-generic/io.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Leipo Yan");
 MODULE_DESCRIPTION("Simple Linux kernel module.");
 MODULE_VERSION("0.01");
 
-#include <linux/phy.h>
-#define PHY_STATE_STR(_state)			\
-	case PHY_##_state:			\
-		return __stringify(_state);	\
-
-static const char *phy_state_to_str(enum phy_state st)
-{
-	switch (st) {
-	PHY_STATE_STR(DOWN)
-	PHY_STATE_STR(READY)
-	PHY_STATE_STR(UP)
-	PHY_STATE_STR(RUNNING)
-	PHY_STATE_STR(NOLINK)
-	PHY_STATE_STR(CABLETEST)
-	PHY_STATE_STR(HALTED)
-	}
-
-	return NULL;
-}
+static void *buf;
+phys_addr_t paddr;
+static int buf_size = 4096;
 
 static int __init lkm_example_init(void) {
-	printk(KERN_INFO "Hello, World!\n");
-	printk(KERN_INFO "%s\n", phy_state_to_str(PHY_NOLINK));
-	WARN(1, "called from state\n");
+	buf = kzalloc(buf_size, GFP_KERNEL);
+	if (buf == NULL) {
+		printk(KERN_ERR "Memory allocation failed\n");
+		return -ENOMEM;
+	}
+
+	paddr = virt_to_phys(buf);
+
+	printk(KERN_INFO "Virtual Address: %p, Physical Address: %pa\n",
+	       buf, &paddr);
+
 	return 0;
 }
 static void __exit lkm_example_exit(void) {
-	printk(KERN_INFO "Goodbye, World!\n");
+	if (buf)
+		kfree(buf);
 }
+
+module_param(buf_size, int, 0600);
+MODULE_PARM_DESC(buf_size, "Buffer size in bytes");
 
 module_init(lkm_example_init);
 module_exit(lkm_example_exit);
