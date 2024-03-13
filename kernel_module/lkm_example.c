c2f7501be4dcb3c6178878a2fc3463f58d0a7b05
Leipo Yan <lyan@maxlinear.com>
2024-03-13 10:49:24 +0800

    Update test module to simulate mac recovery

--- kernel_module/lkm_example.c
+++ kernel_module/lkm_example.c
@@ -9,31 +9,45 @@ MODULE_AUTHOR("Leipo Yan");
 MODULE_DESCRIPTION("Simple Linux kernel module.");
 MODULE_VERSION("0.01");
 
-static void *buf;
-phys_addr_t paddr;
-static int buf_size = 4096;
+#define BM_MARK_OFFSET	64
+
+static int buf_frm_num = 215936;
+static int buf_frm_size = 2048;
+static int pool_start = 0x49210000;
 
 static int __init lkm_example_init(void) {
-	buf = kzalloc(buf_size, GFP_KERNEL);
-	if (buf == NULL) {
-		printk(KERN_ERR "Memory allocation failed\n");
-		return -ENOMEM;
+	int i;
+	unsigned long buff_ptr, ret_pa;
+	unsigned int bm_mark;
+	unsigned long pool_start_low = 0xffff888000000000 | pool_start;
+	unsigned int count = 0;
+
+	printk(KERN_INFO "buf_frm_num: %d buf_frm_size: %d pool_start_low: 0x%lx\n",
+	       buf_frm_num, buf_frm_size, pool_start_low);
+
+	buff_ptr = pool_start_low + BM_MARK_OFFSET;
+	for (i = 0; i < buf_frm_num; i++) {
+		bm_mark = *((u32 *)buff_ptr);
+		if (bm_mark != 0) {
+			count ++;
+			printk(KERN_INFO "count %d bm_mark: 0x%x 0x%lx ret_pa: 0x%lx\n",
+			       count, bm_mark, buff_ptr, ret_pa);
+		}
+		buff_ptr += buf_frm_size;
 	}
-
-	paddr = virt_to_phys(buf);
-
-	printk(KERN_INFO "Virtual Address: %p, Physical Address: %pa\n",
-	       buf, &paddr);
+	printk(KERN_INFO "count %d\n", count);
 
 	return 0;
 }
 static void __exit lkm_example_exit(void) {
-	if (buf)
-		kfree(buf);
 }
 
-module_param(buf_size, int, 0600);
-MODULE_PARM_DESC(buf_size, "Buffer size in bytes");
+module_param(buf_frm_num, int, 0600);
+MODULE_PARM_DESC(buf_frm_num, "Number of buffers");
+module_param(buf_frm_size, int, 0600);
+MODULE_PARM_DESC(buf_frm_size, "Size of buffers");
+module_param(pool_start, int, 0600);
+MODULE_PARM_DESC(pool_start, "Start address of pool");
 
 module_init(lkm_example_init);
 module_exit(lkm_example_exit);
